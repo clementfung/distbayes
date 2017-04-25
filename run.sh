c0ebf3eb15b7bc6df35ca1c4e019fb544b78074b
clementfung <clement.y.fung@gmail.com>
2017-04-24 18:44:46 -0700

    First commit: update run script, works on linux

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 killall xterm
-go build -o client ./client/client_go.go
-go build -o server ./server/server_go.go
-xterm -e ./server 127.0.0.1:2600 s1&
+go build -o client_b ./client/client_go.go
+go build -o server_b ./server/server_go.go
+xterm -e ./server_b 127.0.0.1:6666 s1&
 sleep 2
-xterm -e ./client hospital1 127.0.0.1:2601 127.0.0.1:2600 testdata/x1.txt testdata/y1.txt testdata/xv.txt testdata/yv.txt h1 &
-xterm -e ./client hospital2 127.0.0.1:2602 127.0.0.1:2600 testdata/x2.txt testdata/y2.txt testdata/xv.txt testdata/yv.txt h2 &
-xterm -e ./client hospital3 127.0.0.1:2603 127.0.0.1:2600 testdata/x3.txt testdata/y3.txt testdata/xv.txt testdata/yv.txt h3 &
+xterm -e ./client_b hospital1 127.0.0.1:6664 127.0.0.1:6666 testdata/x1.txt testdata/y1.txt testdata/xv.txt testdata/yv.txt h1 &
+xterm -e ./client_b hospital2 127.0.0.1:6662 127.0.0.1:6666 testdata/x2.txt testdata/y2.txt testdata/xv.txt testdata/yv.txt h2 &
+xterm -e ./client_b hospital3 127.0.0.1:6663 127.0.0.1:6666 testdata/x3.txt testdata/y3.txt testdata/xv.txt testdata/yv.txt h3 &

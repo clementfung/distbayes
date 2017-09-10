9bc53a64e1ac836af01fab725cf30a8bb2ec8547
clementfung <clement.y.fung@gmail.com>
2017-09-09 17:27:58 -0700

    Adding support for multiple studies: separate curator and server

--- experimental/distML_system/DistSys/torserver.go
+++ experimental/distML_system/DistSys/torserver.go
@@ -11,39 +11,47 @@ import (
 )
 
 type MessageData struct {
-	Type 	string
-	Node 	NodeInfo
-	Deltas 	[]float64
+	Type 		  string
+	SourceNode    string
+	Study         StudyInfo
+	Deltas 		  []float64
 }
 
-type NodeInfo struct {
-	NodeId	 	string
+type StudyInfo struct {
+	StudyId	 	string
 	NumFeatures int
+}
+
+// An active study: list of participants and weights
+type Study struct {
+	Participants map[string]string // use a map just because its easier to search through
+	Weights 	 []float64
 }
 
 var (
 	maxnode     		int = 0
 	numFeatures			int = 0
-	myWeights 			[]float64
 	registeredNodes		map[string]string
+	myStudies 			map[string]Study
 
 	// Test Module for python
 	testModule  *python.PyObject
 	testFunc    *python.PyObject
-	trainFunc    *python.PyObject
-
+	trainFunc   *python.PyObject
 )
 
+/*
+	Executes when the .onion domain is accessed via the TorBrowser
+*/
 func handler(w http.ResponseWriter, r *http.Request) {
 
 	req := r.URL.Path[1:]
     
     fmt.Fprintf(w, "Welcome %s!\n\n", r.URL.Path[1:])
-    fmt.Fprintf(w, "Num nodes: %d\n", maxnode)
-    fmt.Fprintf(w, "Weights: %v\n", myWeights)
 
-    if req == "test" {
-    	train_error, test_error := testModel()
+    study, exists := myStudies[req]
+    if exists {
+    	train_error, test_error := testModel(study)
     	fmt.Fprintf(w, "Train Loss: %f\n", train_error)	
     	fmt.Fprintf(w, "Test Loss: %f\n", test_error)	
     }
@@ -57,6 +65,10 @@ func httpHandler() {
 	fmt.Printf("HTTP initialized.\n")
 }
 
+
+/*
+	Automatically used by GoPython to establish function bindings
+*/
 func init() {
 	err := python.Initialize()
 	if err != nil {
@@ -64,6 +76,9 @@ func init() {
 	}
 }
 
+/*
+	Initializes python function objects  
+*/
 func pyInit() {
 
 	sysPath := python.PySys_GetObject("path")
@@ -73,7 +88,6 @@ func pyInit() {
 	testModule = python.PyImport_ImportModule("logistic_model_test")
 	trainFunc = testModule.GetAttrString("train_error")
 	testFunc = testModule.GetAttrString("test_error")
-
 }
 
 func main() {
@@ -82,19 +96,22 @@ func main() {
 
 	pyInit()
 	
+	// setup the handler for web requests through the browser
 	go httpHandler()
-	go tcpSetup("127.0.0.1:5005")
+
+	// setup the handler for the hidden service endpoint
+	go runRouter("127.0.0.1:5005")
 
     // Keeps server running
 	select {}
 }
 
 // Sets up the TCP connection, and attaches GoVector.
-func tcpSetup(address string) {
+func runRouter(address string) {
 
 	Logger := govec.InitGoVector("torserver", "torserverfile")
 
-	// listen on all interfaces
+	// Convert address string to net.Address
 	myaddr, err := net.ResolveTCPAddr("tcp", address)
 	checkError(err)
 
@@ -104,6 +121,10 @@ func tcpSetup(address string) {
 	buf := make([]byte, 2048)
 	outBuf := make([]byte, 2048)
 	registeredNodes = make(map[string]string)
+	myStudies = make(map[string]Study)
+
+	// TODO REMOVE
+	// fmt.Printf("Start sample study: %t\n", startStudy("study1", 101))
 
 	for {
 
@@ -123,47 +144,56 @@ func tcpSetup(address string) {
 		var ok bool
 
 		switch incomingData.Type {
-			
-			// Client requests copy of model, return myWeights
-			case "req":
-				outBuf = Logger.PrepareSend("Replying", myWeights)
 
 			// Client is sending a gradient update. Apply it and return myWeights
 			case "grad":
-				ok = gradUpdate(incomingData.Node.NodeId, incomingData.Deltas)
-				outBuf = Logger.PrepareSend("Replying", myWeights)
+				ok = gradientUpdate(incomingData.SourceNode, 
+					incomingData.Study.StudyId, 
+					incomingData.Deltas)
+
+				outBuf = Logger.PrepareSend("Replying", 
+					myStudies[incomingData.Study.StudyId].Weights)
 			
 			// Add new nodes
 			case "join":
-				
-				ok = processJoin(incomingData.Node)
-
+				ok = processJoin(incomingData.SourceNode, incomingData.Study)
 				if ok {
 			  		outBuf = Logger.PrepareSend("Replying", 1)
 				} else {
 					outBuf = Logger.PrepareSend("Replying", 0)
 				}
 
-			case "beat":
+			// curate a new study
+			case "curator":
+				ok = startStudy(incomingData.Study.StudyId, 
+					incomingData.Study.NumFeatures)
+				if ok {
+			  		outBuf = Logger.PrepareSend("Replying", 1)
+				} else {
+					outBuf = Logger.PrepareSend("Replying", 0)
+				}				
 
-				fmt.Printf("Heartbeat from %s\n", incomingData.Node)
+			case "beat":
+				fmt.Printf("Heartbeat from %s\n", incomingData.SourceNode)
 				outBuf = Logger.PrepareSend("Replying to heartbeat", 1)
 
 			default:
+				fmt.Println("Got a message type I dont recognize.")
 				ok = false
 				outBuf = nil
 				
 		}
 
 	  	conn.Write(outBuf)
-		fmt.Printf("Done processing data from %s\n", incomingData.Node.NodeId)
+		fmt.Printf("Done processing data from %s\n", incomingData.SourceNode)
 
 	}
 
 }
 
-func testModel() (float64, float64) {
+func testModel(study Study) (float64, float64) {
 
+	myWeights := study.Weights 
 	argArray := python.PyList_New(len(myWeights))
 
 	for i := 0; i < len(myWeights); i++ {
@@ -177,55 +207,74 @@ func testModel() (float64, float64) {
 	train_err := python.PyFloat_AsDouble(train_result)
 	
 	return train_err, test_err
-
 }
 
-func processJoin(node NodeInfo) bool {
+func startStudy(studyId string, numFeatures int) bool {
 
-	_, exists := registeredNodes[node.NodeId]
-	
+	_, exists := myStudies[studyId]
 	if exists {
-		fmt.Printf("Rejected a join from: %s\n", node.NodeId)
+		fmt.Printf("Study %s already exists: \n", studyId)
 		return false
 	}
 
-	// After the first registration: set the number of features. Fail otherwise
-	if numFeatures != 0 && numFeatures != node.NumFeatures {
+	// Add study to map and set random weights
+	var newStudy Study
+
+	newStudy.Participants = make(map[string]string)
+	newStudy.Weights = make([]float64, numFeatures)
+	for i := 0; i < numFeatures; i++ {
+		newStudy.Weights[i] = rand.Float64()
+	}
+
+	myStudies[studyId] = newStudy
+	fmt.Printf("Added a new study: %s\n", studyId)
+
+	return true
+}
+
+func processJoin(node string, study StudyInfo) bool {
+
+	// check if study exists
+	theStudy, exists := myStudies[study.StudyId]
+	if !exists {
+		fmt.Printf("Rejected a fake join for study: %s\n", study.StudyId)
 		return false
-	} else if numFeatures == 0 {
-		
-		numFeatures = node.NumFeatures
-		myWeights = make([]float64, node.NumFeatures)
+	}
 
-		for i := 0; i < numFeatures; i++ {
-			myWeights[i] = rand.Float64()
- 		}
-
+	_, exists = theStudy.Participants[node]
+	if exists {
+		fmt.Printf("Node %s is already joined in study: %s \n", node, study.StudyId)
+		return false
 	}
 
 	// Add node
-	registeredNodes[node.NodeId] = "in" 
-	fmt.Println("Joined " + node.NodeId)
-	maxnode++
+	theStudy.Participants[node] = "in" 
+	fmt.Printf("Joined %s in study %s \n", node, study.StudyId)
 
 	return true
-
 }
 
-func gradUpdate(nodeId string, deltas []float64) bool {
+func gradientUpdate(nodeId string, studyId string, deltas []float64) bool {
 
-	_, exists := registeredNodes[nodeId]
-	
-	//fmt.Printf("Deltas: %v", deltas)
+	_, exists := myStudies[studyId]
+	if exists {
 
-	if exists { 
+		_, exists = myStudies[studyId].Participants[nodeId]
+		
 		// Add in the deltas
-		for j := 0; j < len(deltas); j++ {
-			myWeights[j] += deltas[j]
+		if exists { 
+
+			theStudy := myStudies[studyId]
+
+			for j := 0; j < len(deltas); j++ {
+				theStudy.Weights[j] += deltas[j]
+			}
+
+			myStudies[studyId] = theStudy
 		}
 	}
 
-	fmt.Printf("Grad update from: %s %t \n", nodeId, exists)
+	fmt.Printf("Grad update from %s on %s %t \n", nodeId, studyId, exists)
 	return exists
 
 }
